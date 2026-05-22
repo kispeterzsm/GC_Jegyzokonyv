4e4a9a13864f1020e9eb1bbc77eceef49870b73d
kispeterzsm <kispeterzsm@gmail.com>
2026-05-22 14:36:43 +0200

    Release v0.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "hu.gc.jegyzokonyv"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "0.5.4"
+        versionCode = 12
+        versionName = "0.5.5"
 
         vectorDrawables { useSupportLibrary = true }
     }
